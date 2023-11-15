1e7a6b0177fde343e59cbc6161a5f64a9308377e
seoyeon <wjstjdus96@naver.com>
2023-11-16 01:54:20 +0900

    feat: Home의 동아리 리스트인 HomeClubList 컴포넌트 생성

---
+++ src/components/home/HomeClubList.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import ProfileAndName from "../global/ProfileAndName";
+import { FaPlus } from "react-icons/fa";
+
+export default function HomeClubList() {
+  return (
+    <HomeClubListLayout>
+      <ClubListBox>
+        <ProfileAndName name="동아리1" />
+        <ProfileAndName name="동아리2" />
+      </ClubListBox>
+      <AddClubButton>
+        <FaPlus />
+      </AddClubButton>
+    </HomeClubListLayout>
+  );
+}
+
+const HomeClubListLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+
+const AddClubButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #3f3e3e;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  font-size: 20px;
+`;
+
+const ClubListBox = styled.div`
+  display: flex;
+  gap: 20px;
+`;
