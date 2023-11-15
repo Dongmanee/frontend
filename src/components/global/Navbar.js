e42baf2d8727159f389aa2639da599d54af26bd8
seoyeon <wjstjdus96@naver.com>
2023-11-16 01:09:32 +0900

    feat: Navbar 페이지별로 아이콘 선택되도록 구현

--- src/components/global/Navbar.js
+++ src/components/global/Navbar.js
@@ -1,18 +1,34 @@
 import {
   FaHouse,
   FaRegUser,
+  FaUser,
   FaRegCalendar,
+  FaCalendar,
   FaRegCommentDots,
+  FaCommentDots,
 } from "react-icons/fa6";
 import styled from "styled-components";
+import { PiHouseBold, PiHouseFill } from "react-icons/pi";
 
-export default function Navbar() {
+export default function Navbar({ currentPage }) {
   return (
     <NavbarLayout>
-      <FaHouse size={24} />
-      <FaRegCommentDots size={24} />
-      <FaRegCalendar size={24} />
-      <FaRegUser size={24} />
+      {currentPage == "home" ? (
+        <PiHouseFill size={26} />
+      ) : (
+        <PiHouseBold size={26} />
+      )}
+      {currentPage == "chat" ? (
+        <FaCommentDots size={24} />
+      ) : (
+        <FaRegCommentDots size={24} />
+      )}
+      {currentPage == "calender" ? (
+        <FaCalendar size={24} />
+      ) : (
+        <FaRegCalendar size={24} />
+      )}
+      {currentPage == "user" ? <FaUser size={24} /> : <FaRegUser size={24} />}
     </NavbarLayout>
   );
 }
