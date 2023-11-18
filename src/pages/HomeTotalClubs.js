c8d701bd7865d7c08aeebd81a49600a2abb99eb1
seoyeon <wjstjdus96@naver.com>
2023-11-18 17:33:02 +0900

    feat: HomeTotalClubs 페이지 동아리 등록 버튼 생성

--- src/pages/HomeTotalClubs.js
+++ src/pages/HomeTotalClubs.js
@@ -4,8 +4,45 @@ import ClubCategory from "../components/home/clubs/ClubCategoryList";
 import CustomInput from "../components/global/CustomInputs";
 import ClubCard from "../components/home/clubs/ClubCard";
 import ClubList from "../components/home/clubs/ClubList";
+import CustomButton from "../components/global/CustomButton";
 
 const temp_clubs = [
+  {
+    name: "Import",
+    intro: "카페에서 다같이 코딩해요",
+    category: "코딩",
+    personnel: 35,
+  },
+  {
+    name: "Import",
+    intro: "카페에서 다같이 코딩해요",
+    category: "코딩",
+    personnel: 35,
+  },
+  {
+    name: "Import",
+    intro: "카페에서 다같이 코딩해요",
+    category: "코딩",
+    personnel: 35,
+  },
+  {
+    name: "Import",
+    intro: "카페에서 다같이 코딩해요",
+    category: "코딩",
+    personnel: 35,
+  },
+  {
+    name: "Import",
+    intro: "카페에서 다같이 코딩해요",
+    category: "코딩",
+    personnel: 35,
+  },
+  {
+    name: "Import",
+    intro: "카페에서 다같이 코딩해요",
+    category: "코딩",
+    personnel: 35,
+  },
   {
     name: "Import",
     intro: "카페에서 다같이 코딩해요",
@@ -49,6 +86,7 @@ export default function HomeTotalClubs() {
           border="0.05rem solid"
         />
         <ClubList clubs={temp_clubs} />
+        <RegisterClubButton>동아리 등록하기</RegisterClubButton>
       </HomeTotalClubsLayout>
     </Layout>
   );
@@ -57,4 +95,18 @@ export default function HomeTotalClubs() {
 const HomeTotalClubsLayout = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const RegisterClubButton = styled.button`
+  height: 7vh;
+  width: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 1rem;
+  border-radius: 1rem;
+  background-color: ${(props) => props.theme.colors.dark.sm};
+  color: white;
+  font-size: ${(props) => props.theme.sizes.lg};
 `;
