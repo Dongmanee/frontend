3b85a108f487d1e42f4f2a9562aaacf1f7de35c0
seoyeon <wjstjdus96@naver.com>
2023-11-19 19:41:11 +0900

    feat: 태그 달기 기능 구현

---
+++ src/components/global/register/RegisterTag.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import CustomInput from "../CustomInputs";
+import SmallTagBox from "../SmallTagBox";
+import RegisterErrorMsg from "./RegisterErrorMsg";
+import RegisterLabel from "./RegisterLabel";
+import { useCallback, useState, useEffect } from "react";
+
+export default function RegisterTag({ label, isRequired, errorMsg }) {
+  const [hashtag, setHashtag] = useState("");
+  const [hashArr, setHashArr] = useState([]);
+
+  useEffect(() => {
+    window.addEventListener("keyup", onKeyUp);
+  }, []);
+
+  const onChangeHashtag = (e) => {
+    setHashtag(e.target.value);
+  };
+
+  const onKeyUp = useCallback(
+    (e) => {
+      const $HashWrapOuter = document.querySelector(".HashWrapOuter");
+      const $HashWrapInner = document.createElement("div");
+      $HashWrapInner.className = "HashWrapInner";
+      $HashWrapInner.addEventListener("click", () => {
+        $HashWrapOuter?.removeChild($HashWrapInner);
+        setHashArr(hashArr.filter((hashtag) => hashtag));
+      });
+      if (e.keyCode === 13 && e.target.value.trim() !== "") {
+        console.log("Enter Key 입력됨!", e.target.value);
+        $HashWrapInner.innerHTML = "#" + e.target.value;
+        $HashWrapOuter?.appendChild($HashWrapInner);
+        setHashArr((hashArr) => [...hashArr, hashtag]);
+        setHashtag("");
+      }
+    },
+    [hashtag, hashArr]
+  );
+
+  return (
+    <div>
+      <RegisterLabel id="tag" label={label} isRequired={isRequired} />
+      <RegisterTagBox className="HashWrap">
+        <CustomInput
+          className="HashInput"
+          id="tag"
+          radius="0.7rem"
+          height="2.5rem"
+          value={hashtag}
+          onChange={onChangeHashtag}
+          onKeyUp={onkeyup}
+        />
+        <RegisteredTagBox className="HashWrapOuter"></RegisteredTagBox>
+      </RegisterTagBox>
+      {errorMsg && <RegisterErrorMsg errorMsg={errorMsg} />}
+    </div>
+  );
+}
+
+const RegisterTagBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+const RegisteredTagBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  & > div {
+    background-color: ${(props) => props.theme.colors.gray.xxs};
+    color: ${(props) => props.theme.colors.gray.md};
+    font-size: 11px;
+    padding: 10px;
+    border-radius: 15px;
+    font-weight: 700;
+  }
+`;
