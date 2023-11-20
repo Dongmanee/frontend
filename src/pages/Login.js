44311466e40302fe3d6ef4c2903eb43926b01532
seoyeon <wjstjdus96@naver.com>
2023-11-20 13:25:16 +0900

    feat: KakaoLogin 컴포넌트 생성

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,6 +5,7 @@ import { flexCenter, flexColumn } from "../styles/global.style";
 import CustomButton from "../components/global/CustomButton";
 import Layout from "../layouts/Layout";
 import LoginForm from "../components/auth/login/LoginForm";
+import KakaoLogin from "../components/auth/login/KakaoLogin";
 
 export default function Login() {
   return (
@@ -12,7 +13,10 @@ export default function Login() {
       <LogoBox>
         <img src={bglogo} />
       </LogoBox>
-      <LoginForm />
+      <LoginBox>
+        <LoginForm />
+        <KakaoLogin />
+      </LoginBox>
     </Layout>
   );
 }
@@ -21,24 +25,7 @@ const LogoBox = styled.div`
   ${flexCenter};
 `;
 
-const ButtonsCol = styled.div`
-  width: 100%;
-  height: 15vh;
-  ${flexColumn};
-  justify-content: space-around;
-  align-items: center;
-
-  @media screen and (min-width: 1000px) {
-    width: 40%;
-  }
-`;
-
-const KakaoBtn = styled.button`
-  width: 45%;
-  height: 3.5rem;
-  ${flexCenter};
-  border: 1px solid #fee500;
-  border-radius: 0.75rem;
-  background-color: #fee500;
-  cursor: pointer;
+const LoginBox = styled.div`
+  ${flexColumn}
+  gap: 2rem;
 `;
