800cff143afbd3cd583cbcc36e404c13dc2eddda
seoyeon <wjstjdus96@naver.com>
2023-11-22 20:29:24 +0900

    feat: 동아리 멤버 관리 추방 모달 생성

---
+++ src/components/clubSetting/members/ResignModal.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import { IoClose, IoTrash } from "react-icons/io5";
+import {
+  flexCenter,
+  flexColumn,
+  flexICenter,
+} from "../../../styles/global.style";
+import { useRef } from "react";
+
+export default function ResignModal({ name, setIsResignModal }) {
+  const modalBackground = useRef();
+  const closeModal = (e) => {
+    if (e.target === modalBackground.current) {
+      setIsResignModal(false);
+    }
+  };
+
+  return (
+    <ResignModalLayout ref={modalBackground} onClick={(e) => closeModal(e)}>
+      <ModalBox>
+        <ModalHeaderBox>정말 {name}님을 추방하시겠습니까?</ModalHeaderBox>
+        <ModalRow>
+          <IoClose size={20} />
+          닫기
+        </ModalRow>
+        <ModalRow>
+          <IoTrash size={20} />
+          추방
+        </ModalRow>
+      </ModalBox>
+    </ResignModalLayout>
+  );
+}
+
+const ResignModalLayout = styled.div`
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 5;
+`;
+
+const ModalBox = styled.div`
+  ${flexColumn}
+  width: 19rem;
+  position: fixed;
+  left: 50%;
+  right: 0;
+  transform: translate(-50%, 130%);
+
+  border-radius: 1rem;
+  background-color: white;
+  z-index: 6;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  & > div:last-child {
+    color: red;
+    padding-top: 0;
+  }
+`;
+
+const ModalHeaderBox = styled.div`
+  padding: 2rem;
+  border-bottom: 1px solid ${(props) => props.theme.colors.gray.xs};
+  font-size: ${(props) => props.theme.sizes.sm};
+  font-weight: ${(props) => props.theme.weights.lg};
+`;
+
+const ModalRow = styled.div`
+  ${flexICenter}
+  gap: 1rem;
+  padding: 2rem 2rem;
+  font-size: ${(props) => props.theme.sizes.sm};
+`;
