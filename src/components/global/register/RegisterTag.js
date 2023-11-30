5503b0cbde8a333892d23a30e88404d0eca0e133
seoyeon <wjstjdus96@naver.com>
2023-11-30 19:27:44 +0900

    fix: 동아리 태그 등록 로직 수정

--- src/components/global/register/RegisterTag.js
+++ src/components/global/register/RegisterTag.js
@@ -4,75 +4,82 @@ import SmallTagBox from "../SmallTagBox";
 import RegisterErrorMsg from "./RegisterErrorMsg";
 import RegisterLabel from "./RegisterLabel";
 import { useCallback, useState, useEffect } from "react";
+import { flexCenter, flexICenter } from "../../../styles/global.style";
 
 export default function RegisterTag({ label, isRequired, errorMsg }) {
-  const [hashtag, setHashtag] = useState("");
-  const [hashArr, setHashArr] = useState([]);
+  const [tags, setTags] = useState([]);
 
-  useEffect(() => {
-    window.addEventListener("keyup", onKeyUp);
-  }, []);
-
-  const onChangeHashtag = (e) => {
-    setHashtag(e.target.value);
+  const handleKeyDown = (e) => {
+    if (e.key != "Enter") return;
+    const value = e.target.value;
+    if (!value.trim()) return;
+    setTags([...tags, value]);
+    e.target.value = "";
   };
 
-  const onKeyUp = useCallback(
-    (e) => {
-      const $HashWrapOuter = document.querySelector(".HashWrapOuter");
-      const $HashWrapInner = document.createElement("div");
-      $HashWrapInner.className = "HashWrapInner";
-      $HashWrapInner.addEventListener("click", () => {
-        $HashWrapOuter?.removeChild($HashWrapInner);
-        setHashArr(hashArr.filter((hashtag) => hashtag));
-      });
-      if (e.keyCode === 13 && e.target.value.trim() !== "") {
-        $HashWrapInner.innerHTML = "#" + e.target.value;
-        $HashWrapOuter?.appendChild($HashWrapInner);
-        setHashArr((hashArr) => [...hashArr, hashtag]);
-        setHashtag("");
-      }
-    },
-    [hashtag, hashArr]
-  );
+  const removeTag = (tagIdx) => {
+    setTags(tags.filter((tag, idx) => idx != tagIdx));
+  };
 
   return (
     <div>
       <RegisterLabel id="tag" label={label} isRequired={isRequired} />
-      <RegisterTagBox className="HashWrap">
-        <CustomInput
-          className="HashInput"
-          id="tag"
-          radius="0.7rem"
-          height="2.5rem"
-          value={hashtag}
-          onChange={onChangeHashtag}
-          onKeyUp={onkeyup}
-          placeholder="작성 후 엔터를 누르면 자동 입력됩니다."
+      <TagInputContainer>
+        {tags.map((tag, idx) => (
+          <TagItem key={idx}>
+            <span className="text">{tag}</span>
+            <span className="close" onClick={() => removeTag(idx)}>
+              &times;
+            </span>
+          </TagItem>
+        ))}
+        <TagInput
+          onKeyDown={handleKeyDown}
+          placeholder={
+            tags.length == 0 ? "엔터를 입력하여 태그를 등록해주세요" : ""
+          }
         />
-        <RegisteredTagBox className="HashWrapOuter"></RegisteredTagBox>
-      </RegisterTagBox>
+      </TagInputContainer>
       {errorMsg && <RegisterErrorMsg errorMsg={errorMsg} />}
     </div>
   );
 }
 
-const RegisterTagBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
+const TagInputContainer = styled.div`
+  ${flexICenter}
+  flex-wrap: wrap;
+  gap: 0.5em;
+  background-color: ${(props) => props.theme.colors.gray.xxs};
+  padding: 0.4rem 1rem;
+  border-radius: 0.7rem;
 `;
 
-const RegisteredTagBox = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0.5rem;
-  & > div {
-    background-color: ${(props) => props.theme.colors.gray.xxs};
-    color: ${(props) => props.theme.colors.gray.md};
-    font-size: 11px;
-    padding: 10px;
-    border-radius: 15px;
-    font-weight: 700;
+const TagItem = styled.div`
+  display: inline-block;
+  padding: 0.5em 0.75em;
+  border-radius: 20px;
+  background-color: ${(props) => props.theme.colors.gray.xs};
+
+  font-size: ${(props) => props.theme.sizes.xs};
+
+  & > span:last-child {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(48, 48, 48);
+    border-radius: 50%;
+    margin-left: 0.3em;
+    font-size: ${(props) => props.theme.sizes.xxs};
+    color: #fff;
+    cursor: pointer;
   }
+`;
+
+const TagInput = styled.input`
+  flex-grow: 1;
+  padding: 0.5em 0;
+  background-color: ${(props) => props.theme.colors.gray.xxs};
+  border: none;
+  outline: none;
+  font-family: "Noto Sans KR", sans-serif;
 `;
