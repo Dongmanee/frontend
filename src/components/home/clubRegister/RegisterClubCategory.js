4882550a85e57ba061b8cecfdbaa4646aa56236e
seoyeon <wjstjdus96@naver.com>
2023-12-02 00:56:19 +0900

    feat: 동아리 카테고리 가로 슬라이더 구현

--- src/components/home/clubRegister/RegisterClubCategory.js
+++ src/components/home/clubRegister/RegisterClubCategory.js
@@ -5,6 +5,7 @@ import { temp_clubs_catgory } from "../../../consts/tempData";
 import ProfileAndName from "../../global/ProfileAndName";
 import RegisterErrorMsg from "../../global/register/RegisterErrorMsg";
 import HorizontalSlider from "../../global/HorizontalSlider";
+import CategoryIcon from "../../global/CategoryIcon";
 
 export default function RegisterClubCategory({ errorMsg }) {
   return (
@@ -12,12 +13,7 @@ export default function RegisterClubCategory({ errorMsg }) {
       <RegisterLabel label="동아리 카테고리" isRequired={true} />
       <HorizontalSlider>
         {temp_clubs_catgory.map((item, idx) => (
-          <ProfileAndName
-            key={idx}
-            img={item.img}
-            name={item.name}
-            isIcon={true}
-          />
+          <CategoryIcon key={idx} img={item.img} name={item.name} />
         ))}
       </HorizontalSlider>
       {errorMsg && <RegisterErrorMsg errorMsg={errorMsg} />}
