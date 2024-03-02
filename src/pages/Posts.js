07517219ee088092f83aaad2360c050a19dd7317
gibeom <101088024+kkb4363@users.noreply.github.com>
2024-03-02 14:36:43 +0800

    feat: 초기 유저 정보 저장

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,9 +1,35 @@
-import styled from "styled-components";
 import HomeClubList from "../components/home/HomeClubList";
 import { temp_total_posts } from "../consts/tempData";
 import Post from "../components/global/postList/Post";
 import Layout from "../components/layouts/Layout";
+import { getMemberDetail, getClubList, getScheduleList } from "../apis/member";
+import { useUserStore } from "../store/user.store";
+
+import styled from "styled-components";
+import { useEffect } from "react";
+
 export default function Posts() {
+  const userStore = useUserStore();
+
+  useEffect(() => {
+    try {
+      getClubList().then((res) => {
+        if (res.status === 200) {
+          userStore.setClubList(res.data.data);
+        }
+      });
+      getMemberDetail().then((res) => {
+        if (res.status === 200) {
+          userStore.setName(res.data.data.name);
+          userStore.setEmail(res.data.data.email);
+        }
+      });
+      getScheduleList().then((res) => console.log(res));
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <Layout headerLeft="logo" isNavbar={true}>
       <HomeClubList />
